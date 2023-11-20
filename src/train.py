ef165cb979cb770d9fd1557342bd7ffa4419aaec
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-11-20 04:12:44 +0000

    update

--- src/train.py
+++ src/train.py
@@ -4,47 +4,57 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
 
+# Start MLflow run
+mlflow.start_run()
+
+# Download NLTK resources
 nltk.download('gutenberg')
 nltk.download('punkt')
-gutenberg_ids = gutenberg.fileids()
-chosen_text_id = 'austen-emma.txt'  # Cambia a tu texto de Jane Austen preferido
 
+# Choose a text by Jane Austen
+gutenberg_ids = gutenberg.fileids()
+chosen_text_id = 'austen-emma.txt'  # Change to your preferred Jane Austen text
+
+# Load data
 def load_data(text_id):
     return gutenberg.raw(text_id)
 
 data = load_data(chosen_text_id)
 
-# Preprocesamiento de datos (en este caso, simplemente dividimos el texto en oraciones)
+# Data preprocessing (in this case, simply splitting the text into sentences)
 sentences = nltk.sent_tokenize(data)
 
-# Creación de etiquetas de ejemplo (clasificación de texto ficticia)
-# En este caso, etiquetamos las oraciones que contienen el nombre "Emma"
+# Creating example labels (dummy text classification)
+# In this case, label sentences containing the name "Emma"
 labels = [1 if 'Emma' in sentence else 0 for sentence in sentences]
 
-# División de datos en conjunto de entrenamiento y conjunto de prueba
+# Split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(sentences, labels, test_size=0.2, random_state=42)
 
-# Vectorización de texto utilizando TF-IDF
+# Text vectorization using TF-IDF
 vectorizer = TfidfVectorizer()
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
-# Entrenamiento del modelo (en este caso, un clasificador Naive Bayes)
+# Model training (in this case, a Naive Bayes classifier)
 classifier = MultinomialNB()
 classifier.fit(X_train_tfidf, y_train)
 
-# Predicciones en el conjunto de prueba
+# Predictions on the test set
 y_pred = classifier.predict(X_test_tfidf)
 
-# Evaluación del modelo
+# Model evaluation
 accuracy = accuracy_score(y_test, y_pred)
 
-# Imprimir la precisión del modelo
+# Print model accuracy
 print(f"Accuracy: {accuracy}")
 
-# Lograr métricas en MLflow
-import mlflow
-with mlflow.start_run():
-    mlflow.log_param("chosen_text_id", chosen_text_id)
-    mlflow.log_metric("accuracy", accuracy)
+# Log parameters and metrics in MLflow
+mlflow.log_param("chosen_text_id", chosen_text_id)
+mlflow.log_metric("accuracy", accuracy)
+
+# End MLflow run
+mlflow.end_run()
