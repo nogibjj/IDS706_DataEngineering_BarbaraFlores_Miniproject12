291dee7dc3d7bd8029c8a2f99a52fbd9938acb06
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-11-20 04:19:23 +0000

    update

--- test_main.py
+++ test_main.py
@@ -1,29 +1,5 @@
-"""
-Test goes here
+def test_default():
+    assert 1 + 1 ==2
 
-"""
-import unittest
-import os
-from mylib.extract import extract
-
-
-class TestExtractFunction(unittest.TestCase):
-
-    def test_extract(self):
-        input_url = "https://raw.githubusercontent.com/nickeubank/practicaldatascience/master/Example_Data/world-small.csv"
-        output_file_path = "data/WorldSmall.csv"
-
-        if os.path.exists(output_file_path):
-            os.remove(output_file_path)
-
-        result = extract(url=input_url, file_path=output_file_path)
-        self.assertEqual(result, output_file_path)
-        self.assertTrue(os.path.exists(output_file_path))
-
-        if os.path.exists(output_file_path):
-            os.remove(output_file_path)
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    test_default()
